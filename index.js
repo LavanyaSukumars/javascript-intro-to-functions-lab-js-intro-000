f94ccf80c79d96e213a13fe4a4647f914e9b0bd2
Lavanya Sukumar <lavanyaslan@gmail.com>
2018-11-07 08:57:24 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -20,10 +20,9 @@ function logWhisper(string)
 
 function sayHiToGrandma(string)
 {
-  string === string.toLowerCase
-  return "I can't hear you!"
-  string === string.toUpperCase
-  return "YES INDEED!"
-  string === "I love you, Grandma"
-  return "I love you, too."
+  var uppercase = "hello"
+  uppercase.toUpperCase() === uppercase
+  var lowercase = "HELLO"
+  lowercase.toLowerCase() === lowercase
+  var mixedCase = 
 }
